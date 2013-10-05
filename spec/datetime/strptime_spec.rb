3505ce8677006acae3134dca42a286cbc4adb665
Felipe Contreras <felipe.contreras@gmail.com>
2013-10-05 17:39:22 -0500

    date: strptime: move tests to datetime
    This is the right location.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- spec/datetime/strptime_spec.rb
+++ spec/datetime/strptime_spec.rb
@@ -6,4 +6,14 @@ describe "DateTime#strptime" do
     DateTime.strptime("2000-04-06T01:01:01+01:00").should == DateTime.civil(2000, 4, 6, 1, 1, 1, '+1')
   end
 
+  it "parses a second number since the Unix Epoch" do
+    DateTime.strptime("-1", "%s").should == DateTime.civil(1969, 12, 31, 23, 59, 59)
+    DateTime.strptime("-86400", "%s").should == DateTime.civil(1969, 12, 31, 0, 0, 0)
+  end
+
+  it "parses a millisecond number since the Unix Epoch" do
+    DateTime.strptime("-999", "%Q").should == DateTime.civil(1969, 12, 31, 23, 59, 59 + 1.to_r/10**3)
+    DateTime.strptime("-1000", "%Q").should == DateTime.civil(1969, 12, 31, 23, 59, 59)
+  end
+
 end
