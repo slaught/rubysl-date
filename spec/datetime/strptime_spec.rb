a0d7f3c4b460b68bb2e948acc5d2d1c90b354d4d
Felipe Contreras <felipe.contreras@gmail.com>
2013-10-05 17:38:33 -0500

    datetime: strptime: add first test
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- spec/datetime/strptime_spec.rb
+++ spec/datetime/strptime_spec.rb
@@ -1,5 +1,9 @@
 require 'date'
 
-describe "DateTime.strptime" do
-  it "needs to be reviewed for spec completeness"
+describe "DateTime#strptime" do
+
+  it "uses the default format" do
+    DateTime.strptime("2000-04-06T01:01:01+01:00").should == DateTime.civil(2000, 4, 6, 1, 1, 1, '+1')
+  end
+
 end
